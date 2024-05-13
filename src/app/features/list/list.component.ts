095cb86210ec1abfd4a1ca94e18ea81b0ac70045
joemarvi <joelson.dh@gmail.com>
2024-05-13 21:16:58 +0800

    feat: second update files

--- src/app/features/list/list.component.ts
+++ src/app/features/list/list.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, Inject} from '@angular/core';
+import { Observable } from 'rxjs';
+import { Products } from '../../models';
 
 @Component({
   selector: 'app-list',
@@ -8,5 +11,15 @@ import { Component } from '@angular/core';
   styleUrl: './list.component.scss'
 })
 export class ListComponent {
+  products: any[] = [];
 
+  // httpClient = Inject(HttpClient);
+
+  constructor(private httpClient: HttpClient){}
+
+  ngOnInit() {
+    this.httpClient.get<any>('/api/products').subscribe((products) => {
+      this.products = this.products;
+    });
+  }
 }
