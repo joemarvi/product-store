78982d49987da26a39d6ce71760245a817fec03e
joemarvi <joelson.dh@gmail.com>
2024-05-14 01:34:22 +0800

    feat: update style Componente Produto

--- src/app/features/create/create.component.ts
+++ src/app/features/create/create.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @Component({
   selector: 'app-create',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, MatFormFieldModule],
   templateUrl: './create.component.html',
   styleUrl: './create.component.scss'
 })
 export class CreateComponent {
+  form = new FormGroup({
+    title: new FormControl<string>('', {
+      nonNullable: true,
+      validators: Validators.required,
+    }),
+  });
 
+  onSubmit(){
+    this.form.controls.title.value;
+  }
 }
