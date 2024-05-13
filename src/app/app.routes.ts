dfab864b28b517f939995c8b888f850fb64fb819
joemarvi <joelson.dh@gmail.com>
2024-05-14 01:04:55 +0800

    refactor: page list component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,12 @@ import { ListComponent } from './features/list/list.component';
 import { CreateComponent } from './features/create/create.component';
 
 export const routes: Routes = [
-    {path: '', component: ListComponent},
-    {path: 'create-product', component: CreateComponent},
+    { path: '', component: ListComponent },
+    {
+        path: 'create-product',
+        loadComponent: () =>
+            import('./features/create/create.component').then(
+                (m) => m.CreateComponent
+            ),
+    }
 ];
