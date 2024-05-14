899597a7968d33a99acb700745e299de3bb0aea8
Joemarvi19 <joelson.dh@gmail.com>
2024-05-14 12:57:53 -0300

    refactor: cria funções resolvers especializadas

---
+++ src/app/shared/resolvers/get-product.resolver.ts
@@ -0,0 +1,9 @@
+import { inject } from "@angular/core";
+import { ProductsService } from "../services/products.service";
+import { ActivatedRouteSnapshot, RouterStateSnapshot } from "@angular/router";
+
+export const getProduct = (
+  route: ActivatedRouteSnapshot) => {
+  const productsService = inject(ProductsService);
+  return productsService.get(route.paramMap.get('id') as string);
+}
